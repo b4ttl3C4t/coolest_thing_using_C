2b9ba0cb236a465964478d85866f643418f9207e
b4ttl3C4t <129575894+b4ttl3C4t@users.noreply.github.com>
2024-02-18 16:55:32 +0800

    Update

--- XOR_model.c
+++ XOR_model.c
@@ -3,5 +3,85 @@
 #include <math.h>
 #include <time.h>
 
-#include "my_math/limit.h"
+#include "my_math/single_variable_calculus.h"
 
+// Multiple input and single output.
+
+static double input_data[][2] = 
+{
+    {0, 0}, 
+    {0, 1}, 
+    {1, 0}, 
+    {1, 1}, 
+};
+
+static double output_data[] = 
+{
+    0, 
+    1, 
+    1, 
+    0, 
+};
+
+static const unsigned int train_count = sizeof(input_data) / sizeof(input_data[0]);
+static const unsigned int generation_times = 100;
+
+static inline double rand_number(void)
+{
+    return (double)rand() / RAND_MAX;
+}
+
+double cost(double w1, double w2)
+{
+    double summaiton = 0.0;
+    for(size_t i = 0; i < train_count; ++i)
+    {
+        double x1 = input_data[i][0];
+        double x2 = input_data[i][1];
+        double y = x1 * w1 + x2 * w2;
+        
+        double diff = y - output_data[i];
+        /* You can replace the following statement with fabs(diff),
+         * but its performance is not that good. */
+        summaiton += diff * diff;
+        
+        //printf("Actual: %lf\t,Expected: %lf\n", y, train[i][1]);
+    }
+    summaiton /= train_count;
+
+    return summaiton;
+}
+
+int main(void)
+{
+    srand(time(NULL));
+    
+    // y = x * w; w: weight; x: input; y = output;
+    double w = rand_number() * 10;
+    double rate = 1e-2;
+    double deri = 0.0;
+    
+    // Train single cell with input data.
+    printf("Summation of difference: %lf\n", cost(w));
+    for(int i = 0; i < generation_times; ++i)
+    {
+        // The derivative (The rate is identical to *h* in numerical derivative).
+        deri = first_symmetric_derivative(w, rate, cost);
+
+        // The differentiation (The differentiation is equal to the production of the derivative and *h*).
+        // The differentiation = The derivative * h = ((f(x+h) - f(x)) / h) * h = f(x+h) - f(x).
+        w -= deri * rate;
+    }
+
+    printf("Summation of difference: %lf\n", cost(w));
+    
+    // Test the weight with input data.
+    printf("Weight: %lf\n", w);
+    for(size_t i = 0; i < train_count; ++i)
+    {
+        double x = input_data[i];
+        double y = x * w;
+
+        printf("Actual: %lf\t,Expected: %lf\n", y, output_data[i]);
+    }
+}
--- linear_model.c
+++ linear_model.c
@@ -5,20 +5,30 @@
 
 #include "my_math/limit.h"
 
-double train[][2] = 
+static double input_data[] = 
 {
-    {0, 0}, 
-    {1, 2}, 
-    {2, 4}, 
-    {3, 6}, 
-    {4, 8}, 
-    {5, 10},
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
 };
 
-static const unsigned int train_count = sizeof(train) / sizeof(train[0]);
+static double output_data[] = 
+{
+    0, 
+    2, 
+    4,
+    6,
+    8,
+    10,
+};
+
+static const unsigned int train_count = sizeof(input_data) / sizeof(input_data[0]);
 static const unsigned int generation_times = 100;
 
-double rand_number(void)
+static inline double rand_number(void)
 {
     return (double)rand() / RAND_MAX;
 }
@@ -28,10 +38,10 @@ double cost(double w)
     double summaiton = 0.0;
     for(size_t i = 0; i < train_count; ++i)
     {
-        double x = train[i][0];
+        double x = input_data[i];
         double y = x * w;
         
-        double diff = y - train[i][1];
+        double diff = y - output_data[i];
         /* You can replace the following statement with fabs(diff),
          * but its performance is not that good. */
         summaiton += diff * diff;
@@ -45,7 +55,7 @@ double cost(double w)
 
 int main(void)
 {
-    srand(69);
+    srand(time(NULL));
     
     // y = x * w; w: weight; x: input; y = output;
     double w = rand_number() * 10;
@@ -70,9 +80,9 @@ int main(void)
     printf("Weight: %lf\n", w);
     for(size_t i = 0; i < train_count; ++i)
     {
-        double x = train[i][0];
+        double x = input_data[i];
         double y = x * w;
 
-        printf("Actual: %lf\t,Expected: %lf\n", y, train[i][1]);
+        printf("Actual: %lf\t,Expected: %lf\n", y, output_data[i]);
     }
 }
